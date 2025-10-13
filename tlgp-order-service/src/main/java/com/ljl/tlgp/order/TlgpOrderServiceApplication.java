31435c463721de213954225efa81d4da3c0514e5
leijunlong <ljl576879444@126.com>
2025-10-13 10:49:03 +0800

    构建微服务基础项目结构

---
+++ tlgp-order-service/src/main/java/com/ljl/tlgp/order/TlgpOrderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ljl.tlgp.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TlgpOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TlgpOrderServiceApplication.class, args);
+    }
+
+}

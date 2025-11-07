07e328e57e2c16a0e89a32acd68c5fafbb0edf56
leijunlong <ljl576879444@126.com>
2025-11-07 15:57:21 +0800

    修改配置文件

---
+++ tlgp-common/src/main/java/com/ljl/tlgp/common/TlgpCommonApplication.java
@@ -0,0 +1,12 @@
+package com.ljl.tlgp.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TlgpCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TlgpCommonApplication.class, args);
+    }
+}
